c35fc59e305617baaaad9e7ba6689b7494b7e328
shmuli <shmulikeller@gmail.com>
2024-04-21 05:30:57 -0400

    added different sports to sports.py

--- leetcodes/sports.py
+++ leetcodes/sports.py
@@ -10,27 +10,42 @@ class ScheduleMaker:
         self.teams = teams
         self.teamsPlayed = {i: [] for i in range(1, len(teams)+1)}
 
+        self.sportOptions = ["baseketball", "football", "hockey", "volleyball"]
+        self.sportsPlayed = {i: [] for i in range(1, len(teams)+1)}
+
         self.teamsPlayedThisRound = []
         self.whoPlayedWhothisRound = {}
+
+        self.sportsUsed = []
+
         self.inAction = False
 
 
-    def _playgame(self, team1, team2):
+    def _playgame(self, team1, team2, sport):
         self.teamsPlayed[team1].append(team2)
         self.teamsPlayed[team2].append(team1)
         self.teamsPlayedThisRound.append(team1)
         self.teamsPlayedThisRound.append(team2)
         self.whoPlayedWhothisRound[team1] = team2
         self.whoPlayedWhothisRound[team2] = team1
+        self.sportsPlayed[team1].append(sport)
+        self.sportsPlayed[team2].append(sport)
+
+        self.sportsUsed.append(sport)
 
 
-    def _unplay(self, team1, team2):
+
+    def _unplay(self, team1, team2, sport):
         self.teamsPlayed[team1].pop(self.teamsPlayed[team1].index(team2))
         self.teamsPlayed[team2].pop(self.teamsPlayed[team2].index(team1))
         self.teamsPlayedThisRound.pop(self.teamsPlayedThisRound.index(team1))
         self.teamsPlayedThisRound.pop(self.teamsPlayedThisRound.index(team2))
         self.whoPlayedWhothisRound[team1] = False
         self.whoPlayedWhothisRound[team2] = False
+        self.sportsPlayed[team1].pop(self.sportsPlayed[team1].index(sport))
+        self.sportsPlayed[team2].pop(self.sportsPlayed[team2].index(sport))
+        
+        self.sportsUsed.pop(self.sportsUsed.index(sport))
         
 
 
@@ -47,19 +62,35 @@ class ScheduleMaker:
                     continue
                 if team1 in self.teamsPlayed[team2] or team2 in self.teamsPlayed[team1]:
                     continue
-                self._playgame(team1, team2)
+                sportsNotPlayed = list(set(self.sportOptions) - set(self.sportsPlayed[team1]) - set(self.sportsPlayed[team2]) - set(self.sportsUsed))
+                if len(sportsNotPlayed) == 0:
+                    continue
+                if len(sportsNotPlayed) < 1:
+                    continue
+                chosenSport = sportsNotPlayed[0]
+                self._playgame(team1, team2, chosenSport)
                 if self._pairTeamsUp():
                     return True
-                self._unplay(team1, team2)
+                self._unplay(team1, team2, chosenSport)
 
         return False
+    
+    def displaySchedule(self, rounds: int):
+        for round in range(1, rounds+1):
+            print("-------"*3, "round", round, "-------"*3)
+            for team in self.teams:
+                print(f"team {team} will play team {self.teamsPlayed[team][round-1]} in {self.sportsPlayed[team][round-1]}")
+        
 
 
 
     def _playRound(self):
         self.teamsPlayedThisRound = []
         self.whoPlayedWhothisRound = {}
-        return self._pairTeamsUp()
+        pairTeams = self._pairTeamsUp()
+        self.sportsUsed = []
+        print(f"{self.sportsPlayed = }")
+        return pairTeams
 
 
     def createScedule(self, activities: int) -> str:
@@ -72,6 +103,9 @@ class ScheduleMaker:
                 print(self.whoPlayedWhothisRound)
 
         self.inAction = False
+
+
+        self.displaySchedule(roundsCanPlay)
         return f"{roundsCanPlay} rounds can be played"
 
 
